3e473e57ef4a63eb5bc189198d1e77fb958e54ec
brogoeh <tgoeh03@gmail.com>
2025-02-20 00:11:32 +0700

    menu mobile updated

--- src/app/Components/MobileNav.js
+++ src/app/Components/MobileNav.js
@@ -11,7 +11,7 @@ export default function MobileNav() {
       <div className="block lg:hidden " onClick={handleClick}>
         <img src="burger.svg" alt="" className="w-8 z-50 shadow-md cursor-pointer hover:bg-slate-400 trasition duration-100 top-10 rounded right-12 fixed bg-slate-200" />
       </div>
-      <div id="menu" className="text-lg shadow fixed grid justify-around py-10 bg-slate-950 justify-items-center w-full h-screen z-20">
+      <div id="menu" className="text-lg shadow hidden fixed grid bg-slate-950 backdrop-blur-sm pt-10 space-y-4 justify-center items-center place-content-start w-full h-screen z-10">
         <Link href="/">Logo</Link>
         <Link href="/">Home</Link>
         <Link href="/project">Project</Link>
