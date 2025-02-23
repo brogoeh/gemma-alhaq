3f699ffa12c680f11b30e852ebc06e23bb83becb
brogoeh <tgoeh03@gmail.com>
2025-02-23 11:17:00 +0700

    update contact pages

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default function Footer() {
   return (
     <>
-      <div className="w-full py-12 text-center text-sm">
-        Allrights Reserved By Gemma &copy; 2025 <span className="block">made with &hearts; in Bekasi</span>
+      <div className="w-full pt-6 pb-6 text-center text-sm flex-1 mx-auto">
+        <div className="place-content-end mb-6 italic font-semibold">Email : gemmaalhaq@gmail.com</div>
+        <hr className="border-gray-800" />
+        <div className="mt-6">
+          Allrights Reserved By Gemma &copy; 2025 <span className="block">made with &hearts; in Bekasi</span>
+        </div>
       </div>
     </>
   );
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -11,7 +11,9 @@ export default function page() {
             <span className="block underline">Rekening BSI</span>
             <span className="block bg-gradient-to-bl from-gray-900 to-slate-900 w-fit mx-auto text-3xl my-5 py-3 px-5 font-bold rounded-md shadow-lg shadow-indigo-900 italic">6363 1111 63</span>
             <span>a/n Generasi Muda Masjid Al-Haq</span>
-            <span className="block mt-10 text-2xl capitalize font-semibold">jazakumullah khairan katsiran" (جَزَاكُمُ اللهُ خَيْرًا كَثِيْرًا).</span>
+            <span className="block mt-10 text-2xl capitalize font-semibold">
+              "jazakumullah khairan katsiran" <span className="block">(جَزَاكُمُ اللهُ خَيْرًا كَثِيْرًا)</span>
+            </span>
           </div>
         </Container>
       </div>
